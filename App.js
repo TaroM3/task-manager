0aab27e3937128bbd4cb228fc24dfc152d2ac398
李澤洋 <taro.melillo@gmail.com>
2024-01-22 08:50:49 -0300

    App.js Text modified

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text style={styles.text}>Hello World!</Text>
+      <Text style={styles.text}>Hola Coder!</Text>
       <StatusBar style="auto" />
     </View>
   );
